1aa77122d8bd7ffd7a65ce68cd3ec21e8ae2f60f
enjinerd <dev@ron.my.id>
2022-03-23 06:02:11 +0000

    feat: :sparkles: song homework playlist

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './data';
 
 function App() {
+  const {
+    name: songName,
+    external_urls: { spotify: songUrl },
+  } = data;
+  const {
+    name: albumName,
+    external_urls: { spotify: albumUrl },
+    images: [{ url: albumImage }],
+  } = data?.album;
+  const {
+    external_urls: { spotify: artistUrl },
+    name: artistName,
+  } = data?.artists[0];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>🎶 PlayliStation</h1>
       </header>
+      <main>
+        <div className="song_details">
+          <div className="description">
+            <img className="song_image" src={albumImage} alt="song_details" />
+            <div className="metadata">
+              <a href={songUrl}>
+                <span className="title">{songName}</span>
+              </a>
+              <a href={artistUrl}>
+                <span className="artist_name">{artistName}</span>
+              </a>
+              <a href={albumUrl} className="album_name">
+                {albumName}
+              </a>
+            </div>
+          </div>
+          <div id="action_btn">
+            <button id="select_button">Select</button>
+          </div>
+        </div>
+      </main>
+      <footer>
+        <p>
+          <span>© KM_G2FE2230_Roni Ardiyanto</span>
+        </p>
+      </footer>
     </div>
   );
 }
