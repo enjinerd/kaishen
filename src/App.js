dbc3f6f2b5aaa95968f81f6201df319467b6dc66
enjinerd <dev@ron.my.id>
2022-03-25 13:19:43 +0700

    refactor: :recycle: refactor SongDetails as components

--- src/App.js
+++ src/App.js
@@ -1,51 +1,16 @@
 import './App.css';
 import data from './data';
+import { SongDetails } from './components';
+import { Header, Footer } from './components/ui';
 
 function App() {
-  const {
-    name: songName,
-    external_urls: { spotify: songUrl },
-  } = data;
-  const {
-    name: albumName,
-    external_urls: { spotify: albumUrl },
-    images: [{ url: albumImage }],
-  } = data?.album;
-  const {
-    external_urls: { spotify: artistUrl },
-    name: artistName,
-  } = data?.artists[0];
   return (
     <div>
-      <header>
-        <h1>🎶 PlayliStation</h1>
-      </header>
+      <Header />
       <main>
-        <div className="song_details">
-          <div className="description">
-            <img className="song_image" src={albumImage} alt="song_details" />
-            <div className="metadata">
-              <a href={songUrl}>
-                <span className="title">{songName}</span>
-              </a>
-              <a href={artistUrl}>
-                <span className="artist_name">{artistName}</span>
-              </a>
-              <a href={albumUrl} className="album_name">
-                {albumName}
-              </a>
-            </div>
-          </div>
-          <div id="action_btn">
-            <button id="select_button">Select</button>
-          </div>
-        </div>
+        <SongDetails data={data} />
       </main>
-      <footer>
-        <p>
-          <span>© KM_G2FE2230_Roni Ardiyanto</span>
-        </p>
-      </footer>
+      <Footer />
     </div>
   );
 }
