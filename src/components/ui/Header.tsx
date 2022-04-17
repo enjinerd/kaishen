0cc20761ec9af36bc29404093998cf6707643e91
enjinerd <dev@ron.my.id>
2022-04-17 22:22:34 +0700

    refactor(ui): change to ts

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,15 +1,22 @@
 import styles from './Header.module.css';
 
-export function Header({ name, profile_img }) {
+type Props = {
+  name: string;
+  profile_img: string;
+};
+
+const Header: React.FC<Props> = ({ name, profile_img }: Props) => {
   return (
     <header className={styles.header}>
       <h1>🎶 PlayliStation</h1>
       {name && (
         <section className={styles.user_profile}>
-          <img src={profile_img} className={styles.profile_img} />
+          <img src={profile_img} className={styles.profile_img} alt="profile_img" />
           <span className={styles.profile_name}>{name}</span>
         </section>
       )}
     </header>
   );
-}
+};
+
+export default Header;
