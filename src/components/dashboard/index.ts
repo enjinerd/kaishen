2bd5bafb8ed907544e32d026bb20751807d6202b
enjinerd <dev@ron.my.id>
2022-04-22 22:18:22 +0700

    test(dashboard/SearchSong) : add test and fireEvent test

--- src/components/dashboard/index.ts
+++ src/components/dashboard/index.ts
@@ -1,3 +1,4 @@
 export { default as SongDetails } from './SongDetails';
 export { default as SongList } from './SongList';
 export { default as PlaylistForm } from './PlaylistForm';
+export { default as SongSearch } from './SongSearch';
